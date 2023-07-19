db94cbe32fc2764c829eb7bf8a3cf234c09f2f7a
Rafael Correia <rafael.correia@vivest.com.br>
2023-07-18 23:59:37 -0300

    Calculo de contribuicao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,20 +2,28 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { RangeValue } from '@ionic/core';
 import { RangeCustomEvent } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule],
 })
 export class HomePage {
   constructor() {}
 
   lastEmittedValue: RangeValue = 0.1;
+  contribuicao = "0";
+  salario = 3000;
+  percentual = 0.1;
 
-  onIonChange(ev: Event) {
+  onIonInput(ev: Event) {
     this.lastEmittedValue = (ev as RangeCustomEvent).detail.value;
+  }
+
+  onIonChange() {
+    this.contribuicao = (this.salario * (this.percentual/100)).toFixed(2);
   }
 }
