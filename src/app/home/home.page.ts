f14d23895c2b68ed3a839931d65608c318775ac1
Rafael Correia <rafael.correia@vivest.com.br>
2023-07-19 14:03:24 -0300

    Inclusão de PIPE PTBR

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,11 @@ import { IonicModule } from '@ionic/angular';
 import { RangeValue } from '@ionic/core';
 import { RangeCustomEvent } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { CurrencyPipe } from '@angular/common'
+registerLocaleData(ptBr);
 
 @Component({
   selector: 'app-home',
@@ -10,12 +15,13 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['home.page.scss'],
   standalone: true,
   imports: [IonicModule, FormsModule],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}, CurrencyPipe]
 })
 export class HomePage {
-  constructor() {}
+  constructor(private currency: CurrencyPipe) {}
 
   lastEmittedValue: RangeValue = 0.1;
-  contribuicao = "0";
+  contribuicao = "";
   salario = 3000;
   percentual = 0.1;
 
@@ -24,6 +30,6 @@ export class HomePage {
   }
 
   onIonChange() {
-    this.contribuicao = (this.salario * (this.percentual/100)).toFixed(2);
+    this.contribuicao = this.currency.transform(this.salario * (this.percentual/100), 'R$')!;
   }
 }
